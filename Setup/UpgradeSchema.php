a13bb62bc1157c18882d1967cad15a34b511bf6e
Jan Biesiada <jan.biesiada.88@gmail.com>
2018-10-25 20:26:34 +0200

    script fix

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -1,29 +1,17 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 
-namespace Magento\Catalog\Setup;
+namespace JBdev\ExternalOrderId\Setup;
 
-use Magento\Catalog\Model\Product\Attribute\Backend\Media\ImageEntryConverter;
-use Magento\Catalog\Model\Product\Exception;
-use Magento\Catalog\Model\ResourceModel\Product\Gallery;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\DB\Ddl\Table;
 
-/**
- * Upgrade the Catalog module DB scheme
- */
+
 class UpgradeSchema implements UpgradeSchemaInterface
 {
     /**
      * {@inheritdoc}
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
@@ -31,29 +19,29 @@ class UpgradeSchema implements UpgradeSchemaInterface
 
         if (version_compare($context->getVersion(), '1.0.1', '<')) {
             $setup->getConnection()
-            ->addColumn(
-                $setup->getTable('sales_order'),
-                'external_order_id',
-                [
-                    'type' => Table::TYPE_TEXT,
-                    'length' => 40,
-                    'comment' =>'External Order Id',
-                    'nullable' => true
-                ]
-            );
+                ->addColumn(
+                    $setup->getTable('sales_order'),
+                    'external_order_id',
+                    [
+                        'type' => Table::TYPE_TEXT,
+                        'length' => 40,
+                        'comment' =>'External Order Id',
+                        'nullable' => true
+                    ]
+                );
 
-        $setup->getConnection()
-            ->addColumn(
-                $setup->getTable('quote'),
-                'external_order_id',
-                [
-                    'type' => Table::TYPE_TEXT,
-                    'length' => 40,
-                    'comment' =>'External Order Id',
-                    'nullable' => true
-                ]
-            );
-        }
+            $setup->getConnection()
+                ->addColumn(
+                    $setup->getTable('quote'),
+                    'external_order_id',
+                    [
+                        'type' => Table::TYPE_TEXT,
+                        'length' => 40,
+                        'comment' =>'External Order Id',
+                        'nullable' => true
+                    ]
+                );
+            }
         $setup->endSetup();
     }
 }
